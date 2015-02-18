545a9b601e0ea2ca0ae546a9c694b1125a6d20b7
Kostas Kokkinopoulos <KKDTP@kostas.eurogrk.prv>
2015-02-18 19:38:31 +0200

    andrika papa

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var mensShoesSizeTextField: UITextField!
+    @IBOutlet weak var mensConvertedShoesSizeLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +24,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertButtonPressed(sender: AnyObject) {
+        
+       let sizeFromTextField = mensShoesSizeTextField.text
+        if (sizeFromTextField == "") {
+            mensShoesSizeTextField.placeholder = "No empty spaces allowed!"
+            return}
+//        let numberFromTextField = sizefromTextField.toInt()
+//        var integerFromTextField = numberFromTextField!
+        
+        let integerFromTextField = sizeFromTextField.toInt()!
+        
+
+        
+        let conversionConstant = 30
+        mensConvertedShoesSizeLabel.hidden = false
+        mensConvertedShoesSizeLabel.text = "\(integerFromTextField + conversionConstant)" + " in European Shoe size!"
+        
+//        integerFromTextField += conversionConstant
+//        mensConvertedShoesSizeLabel.hidden = false
+//        let stringWithUpdatedShowSize = "\(integerFromTextField)"
+//        mensConvertedShoesSizeLabel.text = stringWithUpdatedShowSize
+        mensShoesSizeTextField.text = ""
+        mensShoesSizeTextField.placeholder = "Enter US Shoe Size Here"
+        mensShoesSizeTextField.becomeFirstResponder()
+    }
 
 }
 
